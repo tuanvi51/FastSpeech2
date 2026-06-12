77a5713299568e6f3fc2f33c97dc40b14dd95e81
Tuan Vi <letuanvi1510206@gmail.com>
2026-06-12 18:22:42 +0700

    chore: clean up project structure and configure new repository URL

--- preprocessor/ljspeech.py
+++ preprocessor/ljspeech.py
@@ -8,13 +8,15 @@ from tqdm import tqdm
 from text import _clean_text
 
 
+import os
+
 def prepare_align(config):
     in_dir = config["path"]["corpus_path"]
     out_dir = config["path"]["raw_path"]
     sampling_rate = config["preprocessing"]["audio"]["sampling_rate"]
     max_wav_value = config["preprocessing"]["audio"]["max_wav_value"]
     cleaners = config["preprocessing"]["text"]["text_cleaners"]
-    speaker = "LJSpeech"
+    speaker = os.path.basename(config["path"]["preprocessed_path"])
     with open(os.path.join(in_dir, "metadata.csv"), encoding="utf-8") as f:
         for line in tqdm(f):
             parts = line.strip().split("|")
